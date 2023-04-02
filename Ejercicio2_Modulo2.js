67d6c7badc5ee79958d6c43e2e306a9141a4064d
Aksteban <99277275+Aksteban@users.noreply.github.com>
2023-04-01 22:06:04 -0500

    Add files via upload

---
+++ Ejercicio2_Modulo2.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Jeremy",
+    21,
+    false,
+    new Date(2001, 09, 28),
+    { 
+        titulo: "El monje que vendió su Ferrari",
+        autor: "Robin S. Sharma",
+        fecha: new Date(1996, 0, 1),
+        url: "https://www.amazon.es/dp/B0062XBS32/"
+      },
+  ];
